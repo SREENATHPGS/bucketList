1873433dc2892ce4a83c5ebbac1ec595b7bb6103
sreenath <sreenath@shiftbytes.co.in>
2021-03-27 01:46:24 +0530

    Added chnages to front end just to show wishes.

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,19 +1,9 @@
 function getBucketListItems( account_id, auth ) {
-    return [ 
-    'Climbing the Alps', 
-    'G2 and Everest before 40!.',
-    'Learn to fly a plane.', 
-    'Cross Atlantic.', 
-    'Fall in love.', 
-    'Go out for dating.', 
-    'Start my own business.',
-    'Travel the world',
-    'Reach space',
-    'Walk on the moon.',
-    'Ride an F1 car.']
+    xOb.getData("wish", {"Content-Type":"application/json"},[],updateFeed)
 }
 
-function updateFeed(list_items) {
+function updateFeed(resp) {
+    let list_items = JSON.parse(resp);
     for (let i=0; i<list_items.length; i++) {
         let middle_panel = document.getElementById("middle_panel");
 
@@ -24,27 +14,28 @@ function updateFeed(list_items) {
         message_area.className = "message_area";
 
         let message_paragraph = document.createElement('p');
-        message_paragraph.innerHTML = list_items[i];
+        message_paragraph.innerHTML = list_items[i]["wish"];
         message_area.appendChild(message_paragraph);
-
+        
         feed_div.appendChild(message_area);
-
+        
         let interact_area = document.createElement('div');
         interact_area.className = "interact_area";
-
+        
         let likeButton = document.createElement('button');
         likeButton.innerHTML = 'Like';
         interact_area.appendChild(likeButton);
-
+        
         let metooButton = document.createElement('button');
         metooButton.innerHTML = 'Metoo';
         interact_area.appendChild(metooButton);
-
+        
         feed_div.appendChild(interact_area);
-
+        
         middle_panel.appendChild(feed_div);
+        console.log(feed_div);
     }
 }
 
-let list_items = getBucketListItems('1001', {"password":"123456"});
-updateFeed(list_items);
+getBucketListItems('1001', {"password":"123456"});
+//updateFeed(list_items);
